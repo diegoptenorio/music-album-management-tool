ade815c20fac43acc8a466a61418a4f0c5420326
Diego Tenório <38768837+diegoptenorio@users.noreply.github.com>
2024-01-28 14:15:45 -0300

    debt: Disable draggable image property

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -3,7 +3,11 @@ import "./logo.scss";
 
 export const Logo = () => (
   <div className="logo">
-    <img src={VenturusLogo} alt="Logo Music Management Tool" />
+    <img
+      src={VenturusLogo}
+      alt="Logo Music Management Tool"
+      draggable={false}
+    />
     <h1>Music Management Tool</h1>
   </div>
 );
