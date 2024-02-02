dab9cd0f8d193c4445e060dcec6a76405ffb3587
Diego Tenório <38768837+diegoptenorio@users.noreply.github.com>
2024-02-02 12:04:46 -0300

    debt: Improving table responsiveness

--- src/features/myArtists/myArtistsList.js
+++ src/features/myArtists/myArtistsList.js
@@ -5,29 +5,37 @@ import "./myArtists.scss";
 
 export const MyArtistsList = ({ artistList }) => {
   return (
-    <div className="list">
+    <>
       <table>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Rating</th>
-            <th>Actions</th>
+            <th className="colName">Name</th>
+            <th className="colRating">Rating</th>
+            <th className="colActions">Actions</th>
           </tr>
         </thead>
-        <tbody>
-          {artistList.map((artist) => (
-            <tr key={artist.id}>
-              <td>{artist.name}</td>
-              <td>{artist.rating}</td>
-              <td>
-                <img src={YoutubeLogo} alt="Youtube" draggable={false} />
-                <img src={PencilIcon} alt="Edit" draggable={false} />
-                <img src={TrashIcon} alt="Delete" draggable={false} />
-              </td>
-            </tr>
-          ))}
-        </tbody>
       </table>
-    </div>
+      <div className="list">
+        <table>
+          <tbody>
+            {artistList.map((artist) => (
+              <tr key={artist.id}>
+                <td className="colName" data-label="Name">
+                  {artist.name}
+                </td>
+                <td className="colRating" data-label="Rating">
+                  {artist.rating}
+                </td>
+                <td className="colActions" data-label="Actions">
+                  <img src={YoutubeLogo} alt="Youtube" draggable={false} />
+                  <img src={PencilIcon} alt="Edit" draggable={false} />
+                  <img src={TrashIcon} alt="Delete" draggable={false} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
